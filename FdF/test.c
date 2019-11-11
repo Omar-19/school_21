338177352a3c0a33fd1997355bf0e8b52545a21d
Ferny Yuko <fyuko@at-f4.21-school.ru>
2019-11-11 17:52:17 +0300

    F

--- FdF/test.c
+++ FdF/test.c
@@ -16,16 +16,19 @@ int		some_key(int key)
 
 int	       main(int ac, char **av)
 {
-  void *mlx_ptr;
-  void *win_ptr;
-  void *img_ptr;
-  int i = 0;
+	void *mlx_ptr;
+	void *win_ptr;
+	void *img_ptr;
+	int i = 0;
 
-  mlx_ptr = mlx_init();
-  win_ptr =  mlx_new_window(mlx_ptr, 1000, 1000, "42");
-  while (i++ < 500)
-    mlx_pixel_put(mlx_ptr, win_ptr, i, i, 0xFFF000);
-  mlx_key_hook(win_ptr, some_key, (void*)0);
-  mlx_loop(mlx_ptr);
-  return (0);
+	mlx_ptr = mlx_init();
+	win_ptr =  mlx_new_window(mlx_ptr, 1000, 1000, "42");
+	while (i++ < 1000)
+		mlx_pixel_put(mlx_ptr, win_ptr, i, 500, 0xFFFFFF);
+	i = 0;
+	while (i++ < 1000)
+		mlx_pixel_put(mlx_ptr, win_ptr, 500, i, 0xFFFFFF);
+	mlx_key_hook(win_ptr, some_key, (void*)0);
+	mlx_loop(mlx_ptr);
+	return (0);
 }
